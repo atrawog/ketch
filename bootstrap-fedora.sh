1cbf0377110ab279f41dceb2b4dfda63977a5567
Andreas Trawöger <atrawog@hexaplant.com>
2019-06-20 17:34:28 +0200

    Fedora Bootstrap

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-yum install -y openssh-server ansible
+dnf makecache
+dnf install -y openssh-server ansible curl python3-pip
 systemctl enable --now sshd
-yum update -y
 cat /dev/zero | ssh-keygen -q -N ""
 curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys
 alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+pip3 install invoke certifi hetzner
+dnf upgrade -y
+dnf clean all
